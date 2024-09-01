d6bea0992d747489314263076000ee0170c52af8
Tathagata <tathagatasau321@gmail.com>
2024-09-01 23:58:09 +0530

    first commit

---
+++ src/main/java/com/macro/webapp/Hello.java
@@ -0,0 +1,13 @@
+package com.macro.webapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello I'm Tathagata!!";
+    }
+}
